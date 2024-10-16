9eed2c8c193a04982cda4ea926aebfb49cef902b
concooking <kovardashov@gmail.com>
2024-10-16 17:50:10 +0300

    Выполнил ДЗ-2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
